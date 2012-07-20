27c13a817d3fbdad6a01cff820bc9eac85c0ecf8
avinash.sahu@gmail.com <avinash.sahu@gmail.com@086da5e1-57f2-0d15-1051-0e68031c1ce1>
2012-07-20 16:27:52 +0000

    multicore

--- trunk/src/myFunc.R
+++ trunk/src/myFunc.R
@@ -1,40 +1,86 @@
 goSignificantCluster  <- function(cluster, geneMat, entrezName, pvalueCutoff = 0.5 ){ 
-	library(biclust)
-	library(GOstats)
-	library(org.Mm.eg.db)
-	geneUniverse  <- entrezName[rownames(intM)]
-	geneUniverse.entrez  <- unlist(lapply(names(geneUniverse), function(x) (geneUniverse[[x]][1])))
+  library(biclust)
+  library(GOstats)
+  library(org.Mm.eg.db)
+  geneUniverse  <- entrezName[rownames(intM)]
+  geneUniverse.entrez  <- unlist(lapply(names(geneUniverse), function(x) (geneUniverse[[x]][1])))
 
-	sigBicluter <- 0
-	Allcluster  <- NULL 
-	sigGO  <- NULL
-	GOcategories  <- c("BP","MF", "CC")
-	#for(i in seq(1,20)){
-	for(i in seq(1,cluster@Number)){
-	  print(i)
-	  geneSample  <- entrezName[rownames(geneMat)[cluster@RowxNumber[,i]]]
-	  geneSample.entrez  <- 	unlist(lapply(names(geneSample), function(x) (geneSample[[x]][1])))
-	  for(goCategory in GOcategories){
-	    params <- new("GOHyperGParams", geneIds = as.vector(geneSample.entrez) , universeGeneIds = as.vector(geneUniverse.entrez), annotation="org.Mm.eg.db", ontology = goCategory, pvalueCutoff = pvalueCutoff, conditional = TRUE, testDirection = "over")
-	    hgOver <- hyperGTest(params)
-	    Allcluster  <- c(Allcluster, hgOver)	
-	    df  <-  summary(hgOver)
-	    if (dim(df)[1] > 0 ){
-	      sigBicluter <- sigBicluter + 1
-	    }
-	    #print(sigBicluter)
-	    sigGO  <- c(sigGO, sigCategories(hgOver))
-	  }
+  sigBicluter <- 0
+  Allcluster  <- NULL 
+  sigGO  <- NULL
+  GOcategories  <- c("BP","MF", "CC")
+  #for(i in seq(1,20)){
+  for(i in seq(1,cluster@Number)){
+    print(i)
+    geneSample  <- entrezName[rownames(geneMat)[cluster@RowxNumber[,i]]]
+    geneSample.entrez  <- 	unlist(lapply(names(geneSample), function(x) (geneSample[[x]][1])))
+    for(goCategory in GOcategories){
+      params <- new("GOHyperGParams", geneIds = as.vector(geneSample.entrez) , universeGeneIds = as.vector(geneUniverse.entrez), annotation="org.Mm.eg.db", ontology = goCategory, pvalueCutoff = pvalueCutoff, conditional = TRUE, testDirection = "over")
+      hgOver <- hyperGTest(params)
+      Allcluster  <- c(Allcluster, hgOver)	
+      df  <-  summary(hgOver)
+      if (dim(df)[1] > 0 ){
+	sigBicluter <- sigBicluter + 1
+      }
+      #print(sigBicluter)
+      sigGO  <- c(sigGO, sigCategories(hgOver))
+    }
 
-	}
-	sigBicluter  <- (sigBicluter/(cluster@Number))
-	sigGO  <- unique(sigGO)
-	out  <- NULL
-	out$sigBicluter  <- sigBicluter
-	out$sigGO  <- length(sigGO)
-	out$Allcluster  <-  Allcluster
-	return(out)
+  }
+  sigBicluter  <- (sigBicluter/(cluster@Number))
+  sigGO  <- unique(sigGO)
+  out  <- NULL
+  out$sigBicluter  <- sigBicluter
+  out$sigGO  <- length(sigGO)
+  out$Allcluster  <-  Allcluster
+  return(out)
 }
+goClust <- function(i, geneMat=geneMat, cluster=cluster, entrezName=entrezName, pvalueCutoff)
+{
+  GOcategories  <- c("BP","MF", "CC")
+  geneSample  <- entrezName[rownames(geneMat)[cluster@RowxNumber[,i]]]
+  geneSample.entrez  <- 	unlist(lapply(names(geneSample), function(x) (geneSample[[x]][1])))
+  goCatSigCnt <- 0
+  for(goCategory in GOcategories){
+    params <- new("GOHyperGParams", geneIds = as.vector(geneSample.entrez) , universeGeneIds = as.vector(geneUniverse.entrez), annotation="org.Mm.eg.db", ontology = goCategory, pvalueCutoff = pvalueCutoff, conditional = TRUE, testDirection = "over")
+    hgOver <- hyperGTest(params)
+    Allcluster  <- c(Allcluster, hgOver)	
+    df  <-  summary(hgOver)
+    if (dim(df)[1] > 0 )
+      goCatSigCnt  <- goCatSigCnt + 1
+
+    #print(sigBicluter)
+    sigGO  <- c(sigGO, sigCategories(hgOver))
+  }
+  #if (goCatSigCnt > 0) 
+  #sigBicluter <- sigBicluter + 1
+  return(sigGO)
+
+}
+
+goSignificantClusterMulticore  <- function(cluster, geneMat, entrezName, pvalueCutoff = 0.5 ){ 
+  library(biclust)
+  library(GOstats)
+  library(org.Mm.eg.db)
+  library(multicore)
+  geneUniverse  <- entrezName[rownames(intM)]
+  geneUniverse.entrez  <- unlist(lapply(names(geneUniverse), function(x) (geneUniverse[[x]][1])))
+
+  sigBicluter <- 0
+  Allcluster  <- NULL 
+  sigGO  <- NULL
+  #for(i in seq(1,cluster@Number))
+  clustseq <- seq(1,cluster@Number)
+  Allcluster  <- mclapply(clustseq, goClust, cluster=cluster, entrezName=entrezName, pvalueCutoff ) 
+  sigBicluter  <- (sigBicluter/(cluster@Number))
+  #sigGO  <- unique(sigGO)
+  out  <- NULL
+  #out$sigBicluter  <- sigBicluter
+  #out$sigGO  <- length(sigGO)
+  out$Allcluster  <-  Allcluster
+  return(out)
+}
+
 numSigCluster <- function(out, Number, pThershold=1e-2 )
 {
   sigBicluster  <- 0
@@ -50,28 +96,28 @@ numSigCluster <- function(out, Number, pThershold=1e-2 )
 }
 
 treeApply <- function(x, func, post=NULL, pre=NULL, ...) {
-	require(XML)
-	ans <- NULL
+  require(XML)
+  ans <- NULL
 
-	value <- func(x)
+  value <- func(x)
 
-	if(length(value))
-		ans <- list(value=value)
+  if(length(value))
+    ans <- list(value=value)
 
-	# If there are any component, do a recursive apply on those also.
-	# If the result is non-null
-	if( length(x[["component"]]) > 0 ) {
-		tmp <- lapply(x[["component"]], treeApply, func, ...)
-		if(length(tmp) > 0)
-			ans$children - tmp
-	}
+  # If there are any component, do a recursive apply on those also.
+  # If the result is non-null
+  if( length(x[["component"]]) > 0 ) {
+    tmp <- lapply(x[["component"]], treeApply, func, ...)
+    if(length(tmp) > 0)
+      ans$children - tmp
+  }
 
-	# invoke the post-processing of component hook.
-	if(length(post)) {
-		post(x)
-	}
+  # invoke the post-processing of component hook.
+  if(length(post)) {
+    post(x)
+  }
 
-	invisible(ans)
+  invisible(ans)
 }
 
 #creates an regression over biclustering methods, number of clusters and parameter of bicluster.
@@ -103,6 +149,24 @@ biClustSearch  <- function(intM, entrezName){
 	outC1  <- cbind(outC1 , out)
       }
 
+    }
+  }
+  return(outC1)
+}
+biClustSearchMulticore  <- function(intM, entrezName){
+  outC1  <- NULL
+  numClusterTest  <-  c(25,100,250)
+  minrs <- c(5, 10, 25, 30 ,40)
+  mincs <- c(5, 10, 25, 30 ,40)
+  pvalueCutoff  <- 1e-3
+  for(numCluster in numClusterTest){
+    for(minr in minrs){
+      for(minc in mincs){
+	intMClust <-biclust(intM.binary, method=BCBimax(), minr=minr, minc=minc, number=numCluster)
+	out  <- goSignificantCluster(intMClust, intM, entrezName, pvalueCutoff=pvalueCutoff )
+	out$runParam  <- list(biclustmethod,pvalueCutoff, numCluster )
+	outC1  <- cbind(outC1 , out)
+      }
     }
   }
   return(outC1)
