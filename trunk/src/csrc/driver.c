b87ec559054113d394ab9eec5701e8cf71bf0c4e
dmgaldi@gmail.com <dmgaldi@gmail.com@086da5e1-57f2-0d15-1051-0e68031c1ce1>
2013-09-23 18:35:44 +0000

    Updated driver memory allocation

--- trunk/src/csrc/driver.c
+++ trunk/src/csrc/driver.c
@@ -7,29 +7,49 @@ int main() {
 	int numLeaf = 4;
 
 	/* indices for malloc of 2-d arrays */
-	int i;
+	int i, j;
 
 	HMMT *hmm = malloc(sizeof(HMMT));
+	ForwardConfig *conf = malloc(sizeof(ForwardConfig));
 
+
+	/* Initial memory allocation */
 	double **alpha = (double **) malloc(sizeof(double *) * seqLength);
 	double **alpha2 = (double **) malloc(sizeof(double *) * (seqLength));
 	hmm->AF = (double **) malloc(sizeof(double *) * numStates);
+	hmm->N = numStates;
+	hmm->M = seqLength;
+	hmm->pi = malloc(sizeof(double) * numStates);
+	hmm->B = malloc(sizeof(double) * seqLength);
+	conf->phi = (double **) malloc(sizeof(double *) * seqLength);
+	conf->phi2 = (double **) malloc(sizeof(double *) * seqLength);
 
+
+	/* Initialize 2D Arrays with as many rows as number of states */
 	for (i = 1; i < numStates; i++) {
 		hmm->AF[i] = (double *) malloc(sizeof(double) * (numStates * numStates));
 	}
 
+	/* Initialize 2D Arrays with as many rows as sequence length */
 	for (i = 1; i < seqLength; i++) {
+
+		/* Arrays with as many columns as number of states */
 		alpha[i] = (double *) malloc(sizeof(double) * numStates);
+		conf->phi = (double *) malloc(sizeof(double) * (numStates));
+
+		/*Arrays with as many columns as number of states squared */
 		alpha2[i] = (double *) malloc(sizeof(double) * (numStates * numStates));
+		hmm->B = (double *) malloc(sizeof(double) * (numStates * numStates));
+		conf->phi2 = (double *) malloc(sizeof(double) * (numStates * numStates));
+
+
 
 	}
 
 	hmm->N = numStates;
 	hmm->M = numSymbols;
-	/* O = obsSeq */
 
 
-	ForwardWithScale(hmm, seqLength, O, numLeaf, alpha, double **alpha2, int *P,
-			double *scale1, double *scale2, double **phi, double **phi2);
+
+	//ForwardWithScale(hmm, seqLength, O, numLeaf, alpha,**alpha2, conf);
 }
